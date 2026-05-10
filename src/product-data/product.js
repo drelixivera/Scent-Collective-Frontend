c2e7e47a0a6e7709eafafc9bdf4af9c79ac3a121
pamilerin9900 <pamilerin9900@gmail.com>
2026-05-10 10:34:34 +0100

    Fix pulic files

--- src/product-data/product.js
+++ src/product-data/product.js
@@ -5,16 +5,16 @@ export const productsData = [
     name: "Midnight Oud",
     brand: "Scent Collective",
     price: 120,
-    image: "public/Valentino Uomo Intense Eau De Parfum 100ml Spray for Men-New&Sealed Discontinued 3614272732278 _ eBay.png",
+    image: "Valentino Uomo Intense Eau De Parfum 100ml Spray for Men-New&Sealed Discontinued 3614272732278 _ eBay.png",
     description: "A deep, woody fragrance for evening wear.",
     category: "Woody"
   },
-  {
+  { 
     id: 2,
     name: "Oceanic Mist",
     brand: "Scent Collective",
     price: 85,
-    image: "public/A luxurious and sensual fragrance with a rich.png",
+    image: "A luxurious and sensual fragrance with a rich.png",
     description: "Fresh, citrus notes inspired by the coastline.",
     category: "Floral"
   },
@@ -24,7 +24,7 @@ export const productsData = [
     price: 6500,
     category: "Fresh",
     description: "A bright, citrusy blend of bergamot and neroli, designed to keep you refreshed under the sun.",
-    image: "public/Armaf Club de Nuit Intense Man – The Ultimate Power Fragrance for Men.png"
+    image: "Armaf Club de Nuit Intense Man – The Ultimate Power Fragrance for Men.png"
     },
     {
     _id: "5",
@@ -32,6 +32,6 @@ export const productsData = [
     price: 13500,
     category: "Luxury",
     description: "Rich honey and warm amber notes. A heavy, long-lasting scent for special occasions.",
-    image: "public/Emporio Armani Men's Stronger With You Eau de Toilette Fragrance Collection - Macy's.png"
+    image: "Emporio Armani Men's Stronger With You Eau de Toilette Fragrance Collection - Macy's.png"
 }
 ];
